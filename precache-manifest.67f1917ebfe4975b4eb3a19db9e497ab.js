f16f3d5f6777b49eac5bce5d54e29e60d5b473e3
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-09-13 17:40:20 -0700

    Updates

---
+++ precache-manifest.67f1917ebfe4975b4eb3a19db9e497ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fc579f5c4cc5ad95d3ddc30978cce7",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "6380b6b7ae8781351600",
+    "url": "/react-quiz/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a2371a91f510091a43ef",
+    "url": "/react-quiz/static/js/2.1f960283.chunk.js"
+  },
+  {
+    "revision": "6380b6b7ae8781351600",
+    "url": "/react-quiz/static/js/main.bf9a8f76.chunk.js"
+  },
+  {
+    "revision": "aa39406f9ecb61df1d78",
+    "url": "/react-quiz/static/js/runtime~main.17680cc5.js"
+  }
+]);
